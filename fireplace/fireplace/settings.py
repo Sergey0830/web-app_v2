57e1bdc79a462cc03706e5ff74c110b8c3db0cef
Sergey <braithwithout@gnail.com>
2024-12-05 21:53:06 +0200

    render deploy2

--- fireplace/fireplace/settings.py
+++ fireplace/fireplace/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 STATIC_DIRS=[
     os.path.join(BASE_DIR, 'main', 'static')
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
