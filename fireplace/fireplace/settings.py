befd2fa49a01d0b754434358a8ea1d99a1ec059a
Sergey <braithwithout@gnail.com>
2024-12-05 21:15:25 +0200

    render deploy

--- fireplace/fireplace/settings.py
+++ fireplace/fireplace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$!#p)2irlj7xe_l#zcrs+x@#^$p5%85eag^$711$d$e%fv5&#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fireplace.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['web-app-v2-1.onrender.com', 'localhost']
 
 
 # Application definition
